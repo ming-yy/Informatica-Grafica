d1babab9a974aa2b9ea5dbfb08460056faa2341c
Manu (839304) <839304@unizar.es>
2024-12-30 17:11:00 +0100

    Cambiado el orden de las transformaciones al leer un mesh en gestorPLY

--- gestorPLY.cpp
+++ gestorPLY.cpp
@@ -35,56 +35,45 @@ vector<Triangulo> generarModeloPLY(const string& rutaArchivo, Esfera& boundingSp
     }
 
     cout << "Archivo = " << rutaArchivo << ", Vertices = " << numVertices << ", Triangulos = " << numCaras << endl;
+
     // Leer los vértices
     for (size_t i = 0; i < numVertices; ++i) {
-        //cout << linea << endl;
         getline(archivo, linea);
         std::stringstream ss(linea);
         float x, y, z;
         ss >> x >> y >> z;
-        //cout << "x = " << x << ", y = " << y << ", z = " << z <<  endl;
         Punto p(x,y,z);
         
-        // Primero invertimos (en torno al origen de coordenadas)
-        if(invertirX){ p = Punto(-p.coord[0], p.coord[1], p.coord[2]); }
-        if(invertirY){ p = Punto(p.coord[0], -p.coord[1], p.coord[2]); }
-        if(invertirZ){ p = Punto(p.coord[0], p.coord[1], -p.coord[2]); }
+        
+        if(escala != 1.0f) { p = scale(p, escala, escala, escala); }
 
-        // Luego giramos (en torno al origen de coordenadas)
         if(rotacionX != 0.0f) {p = rotateX(p, rotacionX); }
         if(rotacionY != 0.0f) { p = rotateY(p, rotacionY); }
         if(rotacionZ != 0.0f) { p = rotateZ(p, rotacionZ); }
-        
-        // Luego escalamos
-        if(escala != 1.0f) { p = scale(p, escala, escala, escala); }
 
+        if(invertirX){ p = Punto(-p.coord[0], p.coord[1], p.coord[2]); }
+        if(invertirY){ p = Punto(p.coord[0], -p.coord[1], p.coord[2]); }
+        if(invertirZ){ p = Punto(p.coord[0], p.coord[1], -p.coord[2]); }
         vertices.emplace_back(p);
     }
 
     Esfera esferaActual = minimumBoundingSphere(vertices);
 
-    // Centramos (movemos) los puntos para centrar la boundingBox al nuevo centro
     Punto desplazamiento(centro.coord[0] - esferaActual.centro.coord[0],
                             centro.coord[1] - esferaActual.centro.coord[1],
                             centro.coord[2] - esferaActual.centro.coord[2]);
-
-    // Mover cada punto sumando el desplazamiento
     for (auto& p : vertices) {
         p = translate(p, desplazamiento.coord[0], desplazamiento.coord[1], desplazamiento.coord[2]);
     }
 
     boundingSphere = Esfera(centro, esferaActual.radio);
     
-    
     // Leer las caras (triángulos)
     for (size_t i = 0; i < numCaras; ++i) {
-        //cout << linea << endl;
         getline(archivo, linea);
         std::stringstream ss(linea);
         size_t numIndices, i0, i1, i2;
         ss >> numIndices >> i0 >> i1 >> i2;
-        //cout << "i0 = " << i0 << ", i1 = " << i1 << ", i2 = " << i2 <<  endl;
-        //cout << "x = " << x << ", y = " << y << ", z = " << z <<  endl;
         if (numIndices == 3) { // Verificamos que sea un triángulo
             triangulos.emplace_back(vertices[i0], vertices[i1], vertices[i2]);
         }
